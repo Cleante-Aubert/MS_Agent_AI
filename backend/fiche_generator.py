aac91221e1b4db277418cd91f7d972f0712556c9
Sylvain Saravanane <sylvainsinou78@gmail.com>
2025-04-11 13:06:49 +0200

    add fiche_generator.py for LangChain

---
+++ backend/fiche_generator.py
@@ -0,0 +1,28 @@
+import getpass
+import os
+from langchain_openai import AzureChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.environ.get("AZURE_OPENAI_API_KEY"):
+  os.environ["AZURE_OPENAI_API_KEY"] = getpass.getpass("Enter API key for Azure: ")
+
+model = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+# TODO : Charger le prompt depuis le fichier génération_fiche.txt
+def load_prompt_from_env_hiro(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        prompt = file.read()
+
+template = load_prompt_from_env_hiro("config/prompt/prompt_env_hiro.txt")
+
+
+# TODO : Créer un objet de prompt LangChain pour la génération de la fiche
+
+# TODO : Fonction pour générer la fiche à partir du prompt
+
